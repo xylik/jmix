052be6f5f4221835dd5fd12a4f1f10ff99738ac1
aleksandrovpv <p.aleksandrov@haulmont.net>
2025-07-31 13:23:05 +0400

    Fixing tests

--- jmix-search/search/src/test/java/test_support/BaseSearchTestConfiguration.java
+++ jmix-search/search/src/test/java/test_support/BaseSearchTestConfiguration.java
@@ -29,6 +29,8 @@ import io.jmix.data.DataConfiguration;
 import io.jmix.data.impl.JmixEntityManagerFactoryBean;
 import io.jmix.data.impl.JmixTransactionManager;
 import io.jmix.data.persistence.DbmsSpecifics;
+import io.jmix.dynattr.DynAttrConfiguration;
+import io.jmix.dynattr.DynAttrManager;
 import io.jmix.eclipselink.EclipselinkConfiguration;
 import io.jmix.search.SearchConfiguration;
 import io.jmix.search.SearchProperties;
@@ -56,6 +58,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import org.springframework.lang.Nullable;
 import jakarta.persistence.EntityManagerFactory;
+
 import javax.sql.DataSource;
 
 @Configuration
@@ -64,7 +67,8 @@ import javax.sql.DataSource;
         DataConfiguration.class,
         EclipselinkConfiguration.class,
         SecurityConfiguration.class,
-        SearchConfiguration.class})
+        SearchConfiguration.class,
+        DynAttrConfiguration.class})
 public class BaseSearchTestConfiguration {
 
     @Autowired
@@ -146,5 +150,11 @@ public class BaseSearchTestConfiguration {
     @Bean
     public ClusterApplicationEventChannelSupplier clusterApplicationEventChannelSupplier() {
         return new LocalApplicationEventChannelSupplier();
+    }
+
+    @Bean
+    @Primary
+    public DynAttrManager dynAttrManager(){
+        return new NoopDynAttrManagerImpl();
     }
 }
